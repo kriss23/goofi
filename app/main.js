f030293dff7b93f95588cebfcc54c098752e0c7e
Boris Maric <boro_mo@hotmail.com>
2016-03-19 22:38:24 +0100

    Tizen config

--- app/main.js
+++ app/main.js
@@ -1,28 +1,16 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var App = require('./js/App');
+window.init = function () {
+    var React = require('react');
+    var ReactDOM = require('react-dom');
+    var App = require('./js/App');
 
-window.onload = function () {
-    // TODO:: Do your initialization job
-
-    // add eventListener for tizenhwkey
-    document.addEventListener('tizenhwkey', function(e) {
-        if(e.keyName == "back")
-            try {
-                tizen.application.getCurrentApplication().exit();
-            } catch (ignore) {
-            }
-    });
-
-    // Sample code
-    var textbox = document.querySelector('.contents');
-    textbox.addEventListener("click", function(){
-        box = document.querySelector('#textbox');
-        box.innerHTML = box.innerHTML == "Basic" ? "Sample" : "Basic";
-    });
-};
-
-ReactDOM.render(
+    ReactDOM.render(
     <App />,
-    document.getElementById('main')
-);
+        document.getElementById('main')
+    );
+
+
+    console.log('hello');
+
+}
+
+setTimeout(init, 500);
