340bd77c1eebd092b0615e87ecaf9de65635e8f1
Kristian Mueller <kristian.mueller@crosslend.com>
2016-03-19 22:04:41 +0100

    FEATURE: make globe display depending on timecodes

--- app/js/Map.js
+++ app/js/Map.js
@@ -7,18 +7,24 @@ var Map = React.createClass({
         // set el height and width etc.
 
         var earth;
-        function initialize() {
+        function initializeEarth() {
             earth = new WE.map('earth_div');
             earth.setView([46.8011, 8.2266], 0.2);
-            WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-                attribution: ''
-            }).addTo(earth);
-
+            WE.tileLayer('http://data.webglearth.com/natural-earth-color/{z}/{x}/{y}.jpg', {
+                      tileSize: 256,
+                      bounds: [[-85, -180], [85, 180]],
+                      minZoom: 0,
+                      maxZoom: 16,
+                      attribution: 'WebGLEarth example',
+                      tms: true
+                    }).addTo(earth);
             // Start a simple rotation animation
-            var before = null;
+            var before = 0;
 
             console.log("HEHRREE")
+            earth.setView([46.8011, 8.2266], 0.2);
 
+            /*
             requestAnimationFrame(function animate(now) {
                 var c = earth.getPosition();
                 var elapsed = before? now - before: 0;
@@ -26,14 +32,40 @@ var Map = React.createClass({
                 earth.setCenter([c[0], c[1] + 0.1*(elapsed/30)]);
                 requestAnimationFrame(animate);
             });
+            */
+        }
+
+        function startBackend() {
+            $.get( "/start", function( data ) {
+                $( ".result" ).html( data );
+                console.log("starting backend at timecode 0")
+            });
         }
 
         function pollApi() {
             setTimeout(pollApi, 1000);
-            // console.log("poloing API :-)")
+
+            $.get( "/getLastMessage", function( data ) {
+                $( ".result" ).html( data );
+
+                console.log("poloing API :-): " + data.msg.data.text)
+
+                if (data.msg.data.type == "geo"){
+                    earth.setView([data.msg.data.lat, data.msg.data.long], 0.3);
+
+                    console.log("Got Coordinates lat: " + data.msg.data.lat)
+                    console.log("Got Coordinates long: " + data.msg.data.long)
+                    $( "#earth_div" ).show()
+                } else {
+                    $( "#earth_div" ).hide()
+                }
+
+            });
         }
 
-        initialize()
+        initializeEarth()
+        startBackend()
+
         setTimeout(pollApi, 1000);
     },
 
